e6f89a58f5cf03377abebb74877c91d6aec9b1f9
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-08-31 17:00:43 +0530

    Update FamilyRepo.java

--- src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
+++ src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
@@ -1,9 +1,14 @@
 package com.bigsquare.ShadiPortal.repositories;
 
 import com.bigsquare.ShadiPortal.entities.Family;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FamilyRepo extends JpaRepository<Family,Integer> {
@@ -11,4 +16,21 @@ public interface FamilyRepo extends JpaRepository<Family,Integer> {
 
     public Optional<Family> findByFamilyNameIgnoreCase(String familyName);
 
+
+    @Query("""
+    SELECT f
+    FROM Family f
+    WHERE LOWER(f.familyName)
+          LIKE LOWER(CONCAT('%', :search, '%'))
+       OR CAST(f.id AS string)
+          LIKE CONCAT('%', :search, '%')
+""")
+    Page<Family> findBySearchQuery(
+            @Param("search") String search,
+            Pageable pageable
+    );
+
+
+
+//    Page<Guest> findBySearchQuery(@Param("search") String search, Pageable pageable);
 }
