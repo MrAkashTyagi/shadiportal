bd393a2c32225162efbdbcdd9e59afd4e1d8b0c1
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-04 01:39:09 +0530

    Create ExpenseService.java

---
+++ src/main/java/com/bigsquare/ShadiPortal/services/ExpenseService.java
@@ -0,0 +1,45 @@
+package com.bigsquare.ShadiPortal.services;
+
+import com.bigsquare.ShadiPortal.dto.ExpenseCategorySummaryDto;
+import com.bigsquare.ShadiPortal.dto.ExpenseSummaryDto;
+import com.bigsquare.ShadiPortal.entities.Expense;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ExpenseService {
+
+    // Create
+    Expense createExpense(Expense expense, MultipartFile bill);
+
+    // Update
+    Expense updateExpense(Integer id, Expense expense);
+
+    // Get By Id
+    Expense getExpenseById(Integer id);
+
+    // Get All
+    List<Expense> getAllExpenses();
+
+    // Paginated + Search + Category Filter
+    Page<Expense> getPaginatedExpenses(
+            int page,
+            int size,
+            String search,
+            String category
+    );
+
+    // Delete
+    void deleteExpense(Integer id);
+
+    // Export
+    byte[] exportExpenses();
+
+    ExpenseSummaryDto getExpenseSummary();
+
+    List<ExpenseCategorySummaryDto>
+    getExpenseCategorySummary();
+
+
+}
