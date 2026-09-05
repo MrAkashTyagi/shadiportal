4831c04134fe4ae4ae8f2b8ab3562e7599de5120
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-06 01:35:52 +0530

    Update FamilyRepo.java

--- src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
+++ src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
@@ -11,25 +11,39 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface FamilyRepo extends JpaRepository<Family,Integer> {
+public interface FamilyRepo extends JpaRepository<Family, Integer> {
     Optional<Family> findByFamilyName(String familyName);
 
     public Optional<Family> findByFamilyNameIgnoreCase(String familyName);
 
 
     @Query("""
-    SELECT f
-    FROM Family f
-    WHERE LOWER(f.familyName)
-          LIKE LOWER(CONCAT('%', :search, '%'))
-       OR CAST(f.id AS string)
-          LIKE CONCAT('%', :search, '%')
-""")
+                SELECT f
+                FROM Family f
+                WHERE LOWER(f.familyName)
+                      LIKE LOWER(CONCAT('%', :search, '%'))
+                   OR CAST(f.id AS string)
+                      LIKE CONCAT('%', :search, '%')
+            """)
     Page<Family> findBySearchQuery(
             @Param("search") String search,
             Pageable pageable
     );
 
+
+    @Query("""
+            SELECT COUNT(g)
+            FROM Family f
+            JOIN f.guestList g
+            """)
+    Long getTotalFamilyMembers();
+
+    @Query("""
+            SELECT COALESCE(MAX(SIZE(f.guestList)), 0)
+            FROM Family f
+            """)
+    Integer getLargestFamilySize();
+    
 
 
 //    Page<Guest> findBySearchQuery(@Param("search") String search, Pageable pageable);
