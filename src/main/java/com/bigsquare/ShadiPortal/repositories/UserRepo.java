2c70e4ebd96a87e81b00442ede9ee7e7c535a021
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-10 17:59:47 +0530

    Update UserRepo.java

--- src/main/java/com/bigsquare/ShadiPortal/repositories/UserRepo.java
+++ src/main/java/com/bigsquare/ShadiPortal/repositories/UserRepo.java
@@ -3,5 +3,12 @@ package com.bigsquare.ShadiPortal.repositories;
 import com.bigsquare.ShadiPortal.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(
+            String email
+    );
+
 }
