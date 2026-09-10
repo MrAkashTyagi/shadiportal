4a53a80385ffe174d56ceff0877289c242298726
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-10 18:01:56 +0530

    Create RegisterRequest.java

---
+++ src/main/java/com/bigsquare/ShadiPortal/dto/RegisterRequest.java
@@ -0,0 +1,44 @@
+package com.bigsquare.ShadiPortal.dto;
+
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+
+    public RegisterRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+}
