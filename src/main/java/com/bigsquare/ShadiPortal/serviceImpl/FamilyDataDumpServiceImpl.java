58683b6cf42de1dfde4d89eb0d4c5cc4eb332b7b
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-12 01:42:57 +0530

    Update FamilyDataDumpServiceImpl.java

--- src/main/java/com/bigsquare/ShadiPortal/serviceImpl/FamilyDataDumpServiceImpl.java
+++ src/main/java/com/bigsquare/ShadiPortal/serviceImpl/FamilyDataDumpServiceImpl.java
@@ -1,8 +1,10 @@
 package com.bigsquare.ShadiPortal.serviceImpl;
 
 import com.bigsquare.ShadiPortal.entities.Family;
+import com.bigsquare.ShadiPortal.entities.User;
 import com.bigsquare.ShadiPortal.helper.FamilyHelper;
 import com.bigsquare.ShadiPortal.repositories.FamilyRepo;
+import com.bigsquare.ShadiPortal.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,16 +20,56 @@ public class FamilyDataDumpServiceImpl {
     @Autowired
     private FamilyRepo familyRepo;
 
+    @Autowired
+    private UserRepo userRepo;
+
     //saving data from excel to db
-    public void save(MultipartFile file){
+//    public void save(MultipartFile file, Integer userId){
+//
+//        try {
+//            List<Family> families= FamilyHelper.convertExcelToListOfFamilies(file.getInputStream());
+//            this.familyRepo.saveAll(families);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+    public void save(
+            MultipartFile file,
+            Integer userId
+    ) {
 
         try {
-            List<Family> families= FamilyHelper.convertExcelToListOfFamilies(file.getInputStream());
-            this.familyRepo.saveAll(families);
+
+            User user =
+                    userRepo.findById(userId)
+                            .orElseThrow(() ->
+                                    new RuntimeException(
+                                            "User not found"
+                                    )
+                            );
+
+            List<Family> families =
+                    FamilyHelper.convertExcelToListOfFamilies(
+                            file.getInputStream()
+                    );
+
+            for (Family family : families) {
+
+                family.setId(null);
+
+                family.setUser(user);
+            }
+
+            this.familyRepo.saveAll(
+                    families
+            );
+
         } catch (IOException e) {
+
             e.printStackTrace();
         }
-
     }
 
     public List<Family> getAllGuests(){
