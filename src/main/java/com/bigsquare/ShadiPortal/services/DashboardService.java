6922e4f2d175637b8ef24d59bc07728cac6e3fdd
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-11 17:41:36 +0530

    Update DashboardService.java

--- src/main/java/com/bigsquare/ShadiPortal/services/DashboardService.java
+++ src/main/java/com/bigsquare/ShadiPortal/services/DashboardService.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 public interface DashboardService {
 
-    DashboardSummaryDto getDashboardSummary();
+    DashboardSummaryDto getDashboardSummary(
+            Integer userId
+    );
 
     List<Guest> getRecentGuests();
 
