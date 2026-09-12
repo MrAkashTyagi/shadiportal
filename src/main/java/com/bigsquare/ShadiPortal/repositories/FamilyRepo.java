5e78c778e48adeae565bd0f81c1a2af3b9d7de88
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-13 01:41:08 +0530

    Update FamilyRepo.java

--- src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
+++ src/main/java/com/bigsquare/ShadiPortal/repositories/FamilyRepo.java
@@ -86,5 +86,8 @@ public interface FamilyRepo extends JpaRepository<Family, Integer> {
             @Param("userId") Integer userId
     );
 
+    List<Family> findByUserId(Long userId);
+
+
 //    Page<Guest> findBySearchQuery(@Param("search") String search, Pageable pageable);
 }
