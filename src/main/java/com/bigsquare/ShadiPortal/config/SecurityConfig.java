dc79d77e35190faab38f7f7097925126a10f382e
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-17 21:24:48 +0530

    Update SecurityConfig.java

--- src/main/java/com/bigsquare/ShadiPortal/config/SecurityConfig.java
+++ src/main/java/com/bigsquare/ShadiPortal/config/SecurityConfig.java
@@ -1,25 +1,32 @@
 package com.bigsquare.ShadiPortal.config;
 
-import java.util.List;
-
+import com.bigsquare.ShadiPortal.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationFilter
+            jwtAuthenticationFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 
@@ -29,33 +36,60 @@ public class SecurityConfig {
     ) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
-
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/register",
-                                "/auth/login"
-                        )
-                        .permitAll()
-                        .anyRequest()
-                        .permitAll()
+                .csrf(
+                        csrf -> csrf.disable()
                 )
 
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable());
+                .cors(
+                        Customizer.withDefaults()
+                )
+
+                .sessionManagement(
+                        session ->
+                                session.sessionCreationPolicy(
+                                        SessionCreationPolicy.STATELESS
+                                )
+                )
+
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        "/auth/register",
+                                        "/auth/login",
+                                        "/error"
+                                )
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+
+                .formLogin(
+                        form -> form.disable()
+                )
+
+                .httpBasic(
+                        basic -> basic.disable()
+                )
+
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource
+    corsConfigurationSource() {
 
         CorsConfiguration configuration =
                 new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:4200")
+                List.of(
+                        "http://localhost:4200"
+                )
         );
 
         configuration.setAllowedMethods(
@@ -74,10 +108,14 @@ public class SecurityConfig {
         );
 
         configuration.setExposedHeaders(
-                List.of("Content-Disposition")
+                List.of(
+                        "Content-Disposition"
+                )
         );
 
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(
+                true
+        );
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
