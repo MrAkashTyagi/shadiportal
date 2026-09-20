e8032e0d2b9f71a8f9c5de9f6094b23468ceac13
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-21 01:13:52 +0530

    Create WallMediaDto.java

---
+++ src/main/java/com/bigsquare/ShadiPortal/dto/WallMediaDto.java
@@ -0,0 +1,29 @@
+package com.bigsquare.ShadiPortal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WallMediaDto {
+
+    private Integer id;
+
+    private String originalFileName;
+
+    private String mediaUrl;
+
+    private String contentType;
+
+    private String mediaType;
+
+    private Long fileSize;
+
+    private String caption;
+
+    private LocalDateTime uploadedAt;
+}
