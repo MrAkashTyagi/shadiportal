6587a5b83025401328a205938cd07e4fdc7c1731
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-21 01:10:48 +0530

    Create WallMediaRepo.java

---
+++ src/main/java/com/bigsquare/ShadiPortal/repositories/WallMediaRepo.java
@@ -0,0 +1,24 @@
+package com.bigsquare.ShadiPortal.repositories;
+
+import com.bigsquare.ShadiPortal.entities.WallMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WallMediaRepo
+        extends JpaRepository<WallMedia, Integer> {
+
+    List<WallMedia>
+    findAllByUserIdOrderByUploadedAtDesc(
+            Integer userId
+    );
+
+    Optional<WallMedia>
+    findByIdAndUserId(
+            Integer mediaId,
+            Integer userId
+    );
+}
