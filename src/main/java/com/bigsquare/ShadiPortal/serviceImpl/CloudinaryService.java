fbf4374ed6dc241974f84362f22b609f00c5798d
MrAkashTyagi <34620687+MrAkashTyagi@users.noreply.github.com>
2026-09-23 00:01:27 +0530

    Create CloudinaryService.java

---
+++ src/main/java/com/bigsquare/ShadiPortal/serviceImpl/CloudinaryService.java
@@ -0,0 +1,39 @@
+package com.bigsquare.ShadiPortal.serviceImpl;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public Map uploadFile(
+            MultipartFile file
+    ) {
+
+        try {
+
+            return cloudinary
+                    .uploader()
+                    .upload(
+                            file.getBytes(),
+                            ObjectUtils.emptyMap()
+                    );
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(
+                    "Cloudinary upload failed",
+                    e
+            );
+        }
+    }
+}
